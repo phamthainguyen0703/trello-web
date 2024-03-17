ca8e747a87bb39e84e211ab85d7c5120cd4c5452
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-03-17 23:13:19 +0700

    config rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
-// Updated by trungquandev.com's author on May 13 2023
-// Sample Eslint config for React project
 module.exports = {
   env: { browser: true, es2020: true, node: true },
   extends: [
@@ -12,12 +10,20 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react", "react-hooks", "react-refresh"],
   rules: {
+    //react
     "react-refresh/only-export-components": "warn",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
     "react/prop-types": 0,
     "react/display-name": 0,
-
+    //mui
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: ["@mui/*/*/*"],
+      },
+    ],
+    //common
     "no-console": 1,
     "no-lonely-if": 1,
     "no-unused-vars": 1,
