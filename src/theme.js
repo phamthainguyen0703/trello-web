65dd8d2df28cce5895f2821f88074d4378e612a5
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-03-18 16:10:03 +0700

    handle dark light system mode

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,26 @@
-import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+// import { cyan, deepOrange, orange, teal } from "@mui/material/colors";
 
 // A custom theme for this app
-const theme = createTheme({
-  palette: {
-    mode: "light", // default
-    primary: {
-      main: "#556cd6",
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: "#009688",
+          secondary: "#ff5722",
+        },
+      },
     },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
+    dark: {
+      palette: {
+        primary: {
+          main: "#00bcd4",
+          secondary: "#ff9800",
+        },
+      },
     },
   },
 });
-
 export default theme;
